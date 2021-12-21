958e5230887be89d8f722bcfb1305ebfead6b1e4
Rahul <rahulsingh3118.rs@gmail.com>
2021-12-21 18:23:05 +0530

    consumed api and displaying few data

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const url = 'https://restcountries.com/v3.1/all';
+
+const container = document.querySelector('.container');
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(response[0].name.common);
+        // console.log(response[0].capital[0]);
+        // console.log(response[0].languages[Object.keys(response[0].languages)[0]]);
+        // console.log(response[0].population);
+        // console.log(response[0].region);
+        console.log(data)
+        data.forEach(response => {
+            container.insertAdjacentHTML("beforeend", 
+            `
+            <div class="country-div">
+            <div>
+                <img class="flag" src=${response.flags.svg} alt="">
+            </div>
+            <div class="content">
+                <h1 class="country">${response.name.common}</h1>
+                <span class="capital">${response.capital[0] ? response.capital[0] : "NA" }</span>
+                <div class="data">
+                    <p class="language">${response.languages[Object.keys(response.languages)[0]] ? response.languages[Object.keys(response.languages)[0]] : "NA"}</p>
+                    <p class="currency">$</p>
+                    <p class="population">${response.population}</p>
+                    <p class="region">${response.region}</p>
+                </div>
+            </div>
+        </div>
+        `)
+        })
+    })

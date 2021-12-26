c526258cfb04c066524c209025796bed5d0d7ebd
Rahul <rahulsingh3118.rs@gmail.com>
2021-12-26 19:08:44 +0530

    :bug: fixed the data extraction of languages and currencies

--- js/script.js
+++ js/script.js
@@ -4,40 +4,6 @@ const url = 'https://restcountries.com/v3.1/all';
 const container = document.querySelector('.container');
 
 
-
-// fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         // console.log(response[0].name.common);
-//         // console.log(response[0].capital[0]);
-//         // console.log(response[0].languages[Object.keys(response[0].languages)[0]]);
-//         // console.log(response[0].population);
-//         // console.log(response[0].region);
-//         console.log(data)
-//         data.forEach(response => {
-//             container.insertAdjacentHTML("beforeend", 
-//             `
-//             <div class="country-div">
-//             <div>
-//                 <img class="flag" src=${response.flags.svg} alt="">
-//             </div>
-//             <div class="content">
-//                 <h1 class="country">${response.name.common}</h1>
-//                 <span class="capital">${response.region}</span>
-//                 <div class="data">
-//                     <p class="language">${response.languages[Object.keys(response.languages)[0]] ? response.languages[Object.keys(response.languages)[0]] : "NA"}</p>
-//                     <p class="currency">$</p>
-//                     <p class="population">${response.population}</p>
-                    
-//                 </div>
-//             </div>
-//         </div>
-//         `)
-//         })
-//     })
-
-//     // <p class="region">${response.capital[0] ? response.capital[0] : "NA" }</p>
-
 // Using XMLHttpRequest()
 
 const request = new XMLHttpRequest();
@@ -50,6 +16,38 @@ request.addEventListener('load', () => {
     console.log(this);
     const data = JSON.parse(request.responseText);
     for(let i=0; i<data.length; i++) {
-        console.log(data[i].name.common);
+        try {
+            const [capital] = data[i].capital;
+            const currency = data[i].currencies[Object.keys(data[i].currencies)[0]];
+            const language = data[i].languages[Object.keys(data[i].languages)[0]]
+
+            console.log(data[i]);
+            container.insertAdjacentHTML("beforeend", 
+                `
+                <div class="country-div">
+                    <div>
+                        <img class="flag" src=${data[i].flags.svg} alt="">
+                    </div>
+                    <div class="content">
+                        <h1 class="country">${data[i].name.common}</h1>
+                        <span class="capital">${capital}</span>
+                        <div class="data">
+                            
+                            <p class="currency">${language}</p>
+                            <p class="population">${data[i].population}</p>
+                            <p class="population">${data[i].region}</p>
+                            <p class="population">${currency.name} (${currency.symbol})</p>
+                            
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+        `)
+        
+        } catch(err) {
+            console.log(err.message);
+        }
     }
+        
 })
